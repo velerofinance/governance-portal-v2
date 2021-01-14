af4b20048a625b6f5906518cfb0ab0c8712361cc
Tyler Sorensen <tyler@makerdao.com>
2021-01-13 14:23:06 -1000

    start adding GA

--- lib/analytics.ts
+++ lib/analytics.ts
@@ -1,4 +1,5 @@
 import mixpanel from 'mixpanel-browser';
+import ReactGA from 'react-ga';
 
 const env = process.env.NODE_ENV === 'production' ? 'prod' : 'test';
 
@@ -7,20 +8,25 @@ const config = {
     mixpanel: {
       token: '4ff3f85397ffc3c6b6f0d4120a4ea40a',
       config: { debug: true, ip: false, api_host: 'https://api.mixpanel.com' }
-    }
+    },
+    gaTrackingId: 'TODO'
   },
   prod: {
     mixpanel: {
       token: 'a030d8845e34bfdc11be3d9f3054ad67',
       config: { ip: false, api_host: 'https://api.mixpanel.com' }
-    }
+    },
+    gaTrackingId: 'TODO'
   }
 }[env];
 
 export const mixpanelInit = () => {
-  console.debug(
-    `[Mixpanel] Tracking initialized for ${env} env using ${config.mixpanel.token}`
-  );
+  console.debug(`[Mixpanel] Tracking initialized for ${env} env using ${config.mixpanel.token}`);
   mixpanel.init(config.mixpanel.token, config.mixpanel.config);
-  mixpanel.track('Pageview', { product: 'governance-portal-v2'});
+  mixpanel.track('Pageview', { product: 'governance-portal-v2' });
+};
+
+export const gaInit = () => {
+  console.debug(`[GA] Tracking initialized for ${env} env using ${config.gaTrackingId}`);
+  ReactGA.initialize(config.gaTrackingId);
 };
