df49ef88c0f352a8a9a3224f9aaf50e93358ae55
Tyler Sorensen <tyler@makerdao.com>
2021-08-02 12:40:28 -0700

    start adding some kind of notice about needing to withdraw mkr before delegating

--- components/delegations/modals/InputDelegateMkr.tsx
+++ components/delegations/modals/InputDelegateMkr.tsx
@@ -1,7 +1,10 @@
 import { Button, Box, Flex, Text } from '@theme-ui/components';
+import { Alert } from 'theme-ui';
 import { MKRInput } from 'components/MKRInput';
 import BigNumber from 'bignumber.js';
 import { useState } from 'react';
+import useAccountsStore from 'stores/accounts';
+import { useLockedMkr } from 'lib/hooks';
 
 type Props = {
   title: string;
@@ -22,11 +25,15 @@ export function InputDelegateMkr({
 }: Props): React.ReactElement {
   const [value, setValue] = useState(new BigNumber(0));
 
+  const currentAccount = useAccountsStore(state =>state.currentAccount);
+  const voteProxy = useAccountsStore(state =>
+    currentAccount ? state.proxies[currentAccount.address] : null
+  );
+  const { data: lockedMkr } = useLockedMkr(currentAccount?.address, voteProxy);
   function handleChange(val: BigNumber): void {
     setValue(val);
     onChange(val);
   }
-
   return (
     <Flex
       sx={{ flexDirection: 'column', alignItems: 'center', justifyContent: 'center', textAlign: 'center' }}
@@ -37,6 +44,9 @@ export function InputDelegateMkr({
       <Text sx={{ color: 'secondaryEmphasis', mt: 3 }}>{description}</Text>
       <Box sx={{ mt: 3, width: '20rem' }}>
         <MKRInput value={value} onChange={handleChange} balance={balance} />
+        {balance && lockedMkr && lockedMkr.gt(0) && (
+          <Alert variant="notice">{balance.gt(0) ? `You have an additional ${lockedMkr.toString()} in the voting contract. Do you want to withdraw it first?` : 'You need to withdraw your MKR from the voting contract before it can be delegated'}</Alert>
+        )}
         <Button onClick={onClick} sx={{ width: '100%', mt: 3 }} disabled={!value || value.eq(0)}>
           {buttonLabel}
         </Button>
