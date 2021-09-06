83fda2d6f3e6a051e978f283d96f1625577ee7e2
Tyler Sorensen <tyler@makerdao.com>
2021-09-06 15:40:53 -0700

    use buggyGetTallyRankedChoiceIrv

--- modules/polls/api/fetchPollTally.ts
+++ modules/polls/api/fetchPollTally.ts
@@ -5,5 +5,5 @@ import { RawPollTally } from '../types';
 export async function fetchPollTally(pollId: number, network?: SupportedNetworks): Promise<RawPollTally> {
   const maker = await getMaker(network);
 
-  return maker.service('govPolling').getTallyRankedChoiceIrv(pollId);
+  return maker.service('govPolling').buggyGetTallyRankedChoiceIrv(pollId);
 }
